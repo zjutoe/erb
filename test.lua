9436ba4d240d72a3c03c62328bc20d1e8ed8e2ad
Toe Zju <zjutoe@gmail.com>
2013-08-05 23:29:48 +0800

    branch prediction training WIP

--- test.lua
+++ test.lua
@@ -1,4 +1,5 @@
 local loadelf = require 'luaelf/loadelf'
+local bit = require "bit"
 local elf = loadelf.init()
 local mem = elf.load(arg[1])
 
@@ -8,7 +9,7 @@ function mem.rd(self, addr)
    local v2 = self[addr+2] or 0
    local v3 = self[addr+3] or 0
    if addr % 4 == 0 then
-      -- TODO use ffi.bit to accelerate
+      -- FIXME is it faster to use bit ops?
       return v0 * 2^24 + v1 * 2^16 + v2 * 2^8 + v3
    else
       return nil		-- FIXME raise an exception?
@@ -19,6 +20,20 @@ dofile("bblock.lua")
 
 local bb0 = get_bblock(mem, 0x400220)
 local bb1 = get_bblock(mem, 0x40022c)
-print(string.format("%08x", tonumber(bb0.tail)))
-print(string.format("%08x", tonumber(bb1.tail)))
+local bb2 = get_bblock(mem, 0x400430)
+print(string.format("%08x - %08x", bb0.addr, tonumber(bb0.tail)))
+print(string.format("%08x - %08x", bb1.addr, tonumber(bb1.tail)))
+print(string.format("%08x - %08x", bb2.addr, tonumber(bb2.tail)))
 
+local next_bblock = {}
+
+function train(next_bblock, b0, b1)
+   next_bblock[b0.addr] = b1.addr
+end
+
+train(next_bblock, bb0, bb1)
+train(next_bblock, bb1, bb2)
+
+for k, v in pairs(next_bblock) do
+   print(string.format("%08x -> %08x", k, v))
+end
