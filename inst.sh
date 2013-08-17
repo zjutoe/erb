a13153f739829f785b88984615ef796fb492ba37
Toe Zju <zjutoe.gmail.com>
2013-08-17 11:19:51 -0400

    memory r/w trace by inst.sh

--- inst.sh
+++ inst.sh
@@ -13,40 +13,36 @@ grep -v -e ^f -e '^ ' -e '^CP' -e ^IN: -e ^OP -e ^pc= $flog | while read -r ln; 
 	gpr[$r4]=$v4
     fi
     if [[ $ln = 0x* ]]; then
-	echo -n $ln
+	#echo -n $ln
+	pc=$(cut -f1 -d':' <<< $ln)
 	inst=$(cut -f2 -d':' <<< $ln)
 	read -r op para <<< $inst
-	# echo op $op
-	# echo para $para
 
 	case $op in
 	    'lb' | 'lbu' | 'lh' | 'lhu' | 'll' | 'lw' | 'lwl' | 'lwr' | \
 	    'sb' | 'sbu' | 'sh' | 'shu' | 'ss' | 'sw' | 'sws' | 'swr' )
-		rt=$(cut -f1 -d','<<< $para)
+		#rt=$(cut -f1 -d','<<< $para)
 		rst=$(cut -f2 -d','<<< $para)
 		offset=$(cut -f1 -d'(' <<< $rst)
 		rst=$(cut -f2 -d'(' <<< $rst)
 		base=$(cut -f1 -d')' <<< $rst)
-		echo " : $rt $offset $base"
+		echo "$pc: $offset + 0x${gpr[$base]}"
 		;;
-	    'nop')
-		echo ""
-		;;
-	    *)
-		IFS=, read -r rs rt rd <<< $para
-		echo -n ' :'
-		if [ -n "$rs" ]; then 
-		    echo " $rs" 
-		fi
-		if [ -n "$rt" ]; then 
-		    echo " $rt" 
-		fi
-		if [ -n "$rd" ]; then 
-		    echo " $rd" 
-		fi
-		# if [ -z "$rd" ] && [ -z "$rt" ] && [ -z "$rd" ]; then
-		#     echo ""
-		# fi
+	    # 'nop')
+	    # 	echo ""
+	    # 	;;
+	    # *)
+	    # 	IFS=, read -r rs rt rd <<< $para
+	    # 	echo -n ' :'
+	    # 	if [ -n "$rs" ]; then 
+	    # 	    echo " $rs" 
+	    # 	fi
+	    # 	if [ -n "$rt" ]; then 
+	    # 	    echo " $rt" 
+	    # 	fi
+	    # 	if [ -n "$rd" ]; then 
+	    # 	    echo " $rd" 
+	    # 	fi
 	esac	
     fi
 done
