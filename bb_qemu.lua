e665d8d533686a0503398b5b75426ac760430d35
Toe Zju <zjutoe@gmail.com>
2013-08-20 00:07:27 +0800

    the source addresses of load isnts

--- bb_qemu.lua
+++ bb_qemu.lua
@@ -1,12 +1,46 @@
 #!/usr/bin/env lua
 
-function ld_insts(bb, head, tail)
+local ops_ld = {
+   lb  = 1,  
+   lbu = 1,
+   lh  = 1,
+   lhu = 1,
+   ll  = 1,
+   lw  = 1,
+   lwl = 1,
+   lwr = 1,
+}
+
+local R = {}
+
+function insts_ld(bb, head, tail)
+   local t = {}
    local s = bb:sub(head, tail)
    -- to match the instrucitons in the BB
    for addr, op, para in s:gmatch("(0x%x+):%s+(%w+)%s*([%w,%-%(%)%$]*)\n") do
-      print(addr, op, para)
+      -- print(addr, op, para)
+      if ops_ld[op] then
+	 t[addr] = {op=op, para=para}
+      end
    end
+
+   return t
 end
+
+function addrs_ld(t)
+   local ret = {}
+   for addr, inst in pairs(t) do
+      local op, para = inst.op, inst.para
+      local rt, offset, base = para:match("(%w+),(%-*%d+)%((%w+)%)")
+      -- print(addr, op, rt, offset, base)
+      ret[#ret ＋ 1] ＝ tonumber(offset) + R[base]
+   end
+
+   return ret
+end
+
+-- TODO: for each BB, save the GPR values in R, or try to retrieve the
+-- GPR values on demand according to the addrs of the ld insts
 
 function main()
 
@@ -40,7 +74,8 @@ function main()
       h, t = lines:find(bbpattern, t-3)
       if h == nil then break end
       -- print(lines:sub(h, h+12))
-      ld_insts(lines, h, t-3)
+      local ins = insts_ld(lines, h, t-3)
+      addrs_ld(ins)
       bbnum = bbnum + 1
    end
 
@@ -48,7 +83,7 @@ function main()
    --    bbnum = bbnum + 1
    --    print(bb:sub(1, 13))
    -- end
-   print(bbnum)   
+   --print(bbnum)   
 end
 
 main()
